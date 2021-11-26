eaeb6c0af01e71b4bfebd0a48a766221bed83d93
Alexandre <alexandresantana401@gmail.com>
2021-11-26 14:30:49 -0300

    adding i18n to pt-br

--- app/models/kind.rb
+++ app/models/kind.rb
@@ -1,3 +1,2 @@
 class Kind < ApplicationRecord
-  has_many :contactss, class_name: "contacts", foreign_key: "reference_id"
 end
---
+++ config/initializers/locale.rb
@@ -0,0 +1,2 @@
+#config.i18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+I18n.default_locale = :'pt-BR'
